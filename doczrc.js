cb568a6ccfa7e3d0db00923386dc304683b50601
TuanTV <tuanhanhphuc123@gmail.com>
2019-11-28 11:38:09 +0700

    Change theme codemirror

--- doczrc.js
+++ doczrc.js
@@ -1,7 +1,18 @@
 export default {
+  htmlContext: {
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          href: 'https://codemirror.net/theme/dracula.css',
+        },
+      ],
+    },
+  },
   themeConfig: {
     mode: 'dark',
     linesToScrollEditor: 60,
+    codemirrorTheme: 'dracula',
   },
   menu: [
     "Introduction",
